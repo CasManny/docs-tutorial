1b2c0c2fbf262e6e7c67f9fa3183ea89274ff257
CasManny <chukwusabastine2018@gmail.com>
2025-04-12 20:34:34 +0100

    08: setup convex database

---
+++ convex/documents.ts
@@ -0,0 +1,6 @@
+import { query } from "./_generated/server";
+export const get = query({
+  handler: async (ctx) => {
+    return ctx.db.query("documents").collect();
+  },
+});
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,25 @@
+"use client";
 import Link from "next/link";
 import { HomeNavbar } from "./_components/home-navbar";
 import { TemplateGallery } from "./_components/template-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.get);
+  if (documents === undefined) {
+    return <p className="">Loading...</p>;
+  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 right-0 left-0 z-10 h-16 bg-white">
         <HomeNavbar />
       </div>
       <div className="mt-16">
-       <TemplateGallery />
+        <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
