b65fa16faccc0cc31ac9ea5427cf2d5d4343fd47
CasManny <chukwusabastine2018@gmail.com>
2025-04-15 12:10:39 +0100

    17: set margins in realtime with liveblocks

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -68,7 +68,10 @@ export function Room({ children }: { children: ReactNode }) {
         return await res.json();
       }}
     >
-      <RoomProvider id={params.documentId as string}>
+      <RoomProvider
+        id={params.documentId as string}
+        initialStorage={{ leftMargin: 56, rightMargin: 56 }}
+      >
         <ClientSideSuspense
           fallback={<FullscreenLoader label="Room Loading" />}
         >
--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,11 +1,18 @@
+import { useMutation, useStorage } from "@liveblocks/react";
 import { ArrowDown } from "lucide-react";
 import React, { useRef, useState } from "react";
 
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 export const Ruler = () => {
-  const [leftMargin, setLeftMargin] = useState(56);
-  const [rightMargin, setRightMargin] = useState(56);
+  const rightMargin = useStorage((root) => root.rightMargin) ?? 56;
+  const leftMargin = useStorage((root) => root.leftMargin) ?? 56;
+  const setLeftMargin = useMutation(({ storage }, position: number) => {
+    storage.set("leftMargin", position);
+  }, []);
+  const setRightMargin = useMutation(({ storage }, position: number) => {
+    storage.set("rightMargin", position);
+  }, []);
   const [isDraggingLeft, setisDraggingLeft] = useState(false);
   const [isDraggingRight, setIsDraggingRight] = useState(false);
   const rulerRef = useRef<HTMLDivElement>(null);
